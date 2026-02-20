c25d9f41611508eacf59274125ceae13f2738358
fedorBa <fedrbabushkin2@gmail.com>
2026-02-19 20:29:52 -0500

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,14 +9,13 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
-    const [total, setTotal] = useState(0);
+    let total = 0;
     cart.forEach(item => {
-        const price = parseFloat(item.cost.substring(1));
-        const quantity = item.quantity;
-        total += price * quantity;
+      const price = parseFloat(item.cost.substring(1));
+      total += price * item.quantity;
     });
     return total;
-    };
+  };
 
   const handleContinueShopping = (e) => {
     onContinueShopping(e);
